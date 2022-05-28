0fef3250e6bc69f481b1896b7230a2d6763e5d89
eugene.kalashnikov <eugene.kalashnikov@global.ntt>
2022-05-29 00:06:34 +0200

    review services and controllers;

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 import { prop } from '@typegoose/typegoose';
+import { Types } from 'mongoose';
 
 export interface ReviewModel extends Base {}
 export class ReviewModel extends TimeStamps {
@@ -17,4 +18,7 @@ export class ReviewModel extends TimeStamps {
 
   @prop()
   createdAt: Date;
+
+  @prop()
+  productId: Types.ObjectId;
 }
