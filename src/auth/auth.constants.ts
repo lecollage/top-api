ea4e6039c1b94d46faeb0202383b3883036601d8
eugene.kalashnikov <eugene.kalashnikov@global.ntt>
2022-06-05 16:22:39 +0200

    register, login, JWT token generation;

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,3 @@
+export const ALREADY_REGISTERED_ERROR = 'User is already registered';
+export const USER_NOT_FOUND_ERROR = 'User with this email is not found';
+export const WRONG_PASSWORD_ERROR = 'Wrong password';
