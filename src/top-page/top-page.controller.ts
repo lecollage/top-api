eeb88b037b00e807516a6c928c247cc267929abe
eugene.kalashnikov <eugene.kalashnikov@global.ntt>
2022-06-07 22:29:02 +0200

    top-page controller - 1;

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -8,13 +8,22 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { DocumentType } from '@typegoose/typegoose/lib/types';
+
 import { TopPageModel } from './top-page.model';
 import { FindTopPageDto } from './dto/find-top-page.dto';
+import { TopPageService } from './top-page.service';
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly topPageService: TopPageService) {}
+
   @Post('create')
-  async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
+  async create(
+    @Body() dto: Omit<TopPageModel, '_id'>,
+  ): Promise<DocumentType<TopPageModel>> {
+    return this.topPageService.create(dto);
+  }
 
   @Get(':id')
   async get(@Param('id') id: string) {}
---
+++ src/top-page/top-page.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { DocumentType, ModelType } from '@typegoose/typegoose/lib/types';
+
+import { TopPageModel } from './top-page.model';
+
+@Injectable()
+export class TopPageService {
+  constructor(
+    @InjectModel(TopPageModel)
+    private readonly topPageModel: ModelType<TopPageModel>,
+  ) {}
+
+  async create(
+    dto: Omit<TopPageModel, '_id'>,
+  ): Promise<DocumentType<TopPageModel>> {
+    return this.topPageModel.create(dto);
+  }
+}
